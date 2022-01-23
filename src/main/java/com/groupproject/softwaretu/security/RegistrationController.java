4e9d3c137dd4ec3e14369865b5fa22db13c80e4e
bisrat-walle <bisratwalle3@gmail.com>
2022-01-23 13:00:57 +0300

    fixing register white label error

--- src/main/java/com/groupproject/softwaretu/security/RegistrationController.java
+++ src/main/java/com/groupproject/softwaretu/security/RegistrationController.java
@@ -6,12 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.ui.Model;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/register")
 @RequiredArgsConstructor
+@Slf4j
 public class RegistrationController {
     @Autowired
     private UserRepository userRepository;
@@ -24,9 +28,21 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public String processRegistration(RegistrationForm form){
-        userRepository.save(form.toUser(passwordEncoder));
-        return "redirect:/login";
+    public String processRegistration(RegistrationForm form, Model model){
+		try{
+			userRepository.save(form.toUser(passwordEncoder));
+			return "redirect:/login";
+		} catch(DataIntegrityViolationException ex){
+			log.info("error "+ex.getMessage());
+			model.addAttribute("Integ exception", ex.getMessage());
+			return "redirect:/register?error";
+		} catch(Exception e){
+			log.info("Exception error "+e.getMessage());
+			model.addAttribute("exception", e.getMessage());
+			return "redirect:/register?error";
+		}
+        
+        
     }
 
 }

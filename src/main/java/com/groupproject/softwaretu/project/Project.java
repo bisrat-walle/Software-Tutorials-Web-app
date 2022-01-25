ddb1c06cc7d3dff3199a80de0df00caa47724b40
bisrat-walle <bisratwalle3@gmail.com>
2022-01-25 23:02:21 +0300

    improving Project.java

--- src/main/java/com/groupproject/softwaretu/project/Project.java
+++ src/main/java/com/groupproject/softwaretu/project/Project.java
@@ -26,6 +26,4 @@ public class Project {
     @Column(length = 1000, nullable = false)
     @Size(min = 100, message  = "Problem statement must be at least 100 characters long ")
     private String problemStatement;
-
-	
 }

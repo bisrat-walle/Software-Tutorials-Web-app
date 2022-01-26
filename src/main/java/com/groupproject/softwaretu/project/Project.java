e97627249223c3bc8857876f66a1787b671ac0cb
samiabat <samuelabatneh20@gmail.com>
2022-01-26 23:30:15 +0300

    improving enrollement experience

--- src/main/java/com/groupproject/softwaretu/project/Project.java
+++ src/main/java/com/groupproject/softwaretu/project/Project.java
@@ -17,13 +17,11 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long projectId;
 
-    @NotNull(message = "Project title cannot be null")
-    @Column(length = 30, nullable = false)
-    @Size(min = 5, message  = "Title must be at least 5 characters long ")
+    
+    @Column(length = 50)
     private String title;
 
-    @NotNull(message = "Project content cannot be null")
-    @Column(length = 1000, nullable = false)
-    @Size(min = 10, message  = "Problem statement must be at least 100 characters long ")
+    
+    @Column(length = 1000)
     private String problemStatement;
 }
